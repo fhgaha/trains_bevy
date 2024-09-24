968e750008d227037e831d74e943cd771c460cbc
fhgaha <antsy1992@gmail.com>
2024-09-24 19:44:48 +0300

    for some reason `DefaultPlugins.set(bevy_mod_raycast::low_latency_window_plugin())` unlocks max fps. Removed it so max fps is set to 60.

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,9 @@ use bevy_mod_raycast::cursor::CursorRayPlugin;
 fn main() {
     App::new()
         .add_plugins((
-            DefaultPlugins.set(bevy_mod_raycast::low_latency_window_plugin()),
+            DefaultPlugins
+            // .set(bevy_mod_raycast::low_latency_window_plugin()) //why the hell this unlocks max fps?
+            ,
             trains_plugin::TrainsPlugin,
             fps::FpsPlugin,
             // examples::ecs_example_1::EcsExample1
--- src/trains_plugin.rs
+++ src/trains_plugin.rs
@@ -1,4 +1,4 @@
-use bevy::{math::VectorSpace, prelude::*};
+use bevy::prelude::*;
 use bevy_mod_raycast::prelude::*;
 use bevy_rts_camera::{RtsCamera, RtsCameraControls, RtsCameraPlugin};
 
@@ -8,25 +8,20 @@ impl Plugin for TrainsPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(RtsCameraPlugin)
             .add_systems(Startup, setup)
-            .add_systems(Update, draw_cursor)
-        ;
+            .add_systems(Update, draw_cursor);
     }
 }
 
 #[derive(Component)]
 struct Terrain;
 
-fn setup(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     //mesh
     commands.spawn((
         SceneBundle {
-            scene: asset_server.load(
-                GltfAssetLabel::Scene(0)
-                    .from_asset("../assets/models/Peachs Castle Exterior/gltf/flipped_faces/untitled.glb"),
-            ),
+            scene: asset_server.load(GltfAssetLabel::Scene(0).from_asset(
+                "../assets/models/Peachs Castle Exterior/gltf/flipped_faces/untitled.glb",
+            )),
             ..default()
         },
         Terrain,
@@ -54,23 +49,18 @@ fn draw_cursor(
     mut raycast: Raycast,
 ) {
     let (camera, camera_transform) = cameras.single();
-
-    let ground = terrains.single();
-
+    let terr = terrains.single();
     let Some(cursor_position) = windows.single().cursor_position() else {
         return;
     };
-
     let Some(ray) = camera.viewport_to_world(camera_transform, cursor_position) else {
         return;
     };
-
     let intersections = raycast.debug_cast_ray(ray, &default(), &mut gizmos);
+    let point: Vec3 = match intersections.len() > 0 {
+        true => intersections[0].1.position(),
+        false => Vec3::ZERO,
+    };
 
-    let mut point = Vec3::ZERO;
-    if intersections.len() > 0 {
-        point = intersections[0].1.position();
-    }
-    println!("aaa {point}");
-    gizmos.circle(point + ground.up() * 0.01, ground.up(), 0.2, Color::WHITE);
+    gizmos.circle(point + terr.up() * 0.01, terr.up(), 0.2, Color::WHITE);
 }
